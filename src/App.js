ec35485dcbe910809081bf86cce505c97e833462
kathyavini <kathyavini@gmail.com>
2022-05-03 20:56:57 -0700

    Add scoring and move cards into component files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
+import './Styles/App.css';
 import { useState, useEffect } from 'react';
+import Cards from './Components/Cards';
 import citiesObject from './cities';
 
 // In App:
@@ -22,93 +23,42 @@ function ScoreBoard({ score, bestScore }) {
   );
 }
 
-function Card({ city, index, url, handleClick }) {
-  return (
-    <figure className="city-card" onClick={handleClick}>
-      <img
-        src={url}
-        // On android, the following works in Chrome but not Firefox (sigh) to suppress the long press context menu
-        // onContextMenu={(event) => {
-        //   event.preventDefault();
-        //   event.stopPropagation();
-        // }}
-      />
-      <figcaption>{city}</figcaption>
-    </figure>
-  );
-}
 
-function Cards() {
-  const [history, setHistory] = useState([]);
-
-  const [orderedCities, setOrderedCities] = useState(citiesObject);
-
-  function timeout(delay) {
-    return new Promise((res) => setTimeout(res, delay));
-  }
-
-  useEffect(() => {
-    shuffleCards();
-  }, []);
-
-  useEffect(() => {
-    console.log(history);
-  }, [history]);
-
-  function recordClick(city) {
-    setHistory((prevHistory) => [...prevHistory, city]);
-  }
-
-  async function shuffleCards() {
-    // Fisher-Yates; did not think it up myself
-    function randomSort(array) {
-      // Starting at last element
-      for (let i = array.length - 1; i > 0; i--) {
-        // Pick a random element from earlier in the array
-        let j = Math.floor(Math.random() * (i + 1));
-        // swap with current elements
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    }
-
-    const shuffledCities = randomSort(citiesObject).map((city) => ({
-      ...city,
-    }));
-
-    await timeout(400);
-    setOrderedCities(shuffledCities);
-  }
-
-  const displayCards = orderedCities.map((city) => (
-    <Card
-      city={city.name}
-      key={city.index}
-      index={city.index}
-      url={city.url}
-      handleClick={() => {
-        recordClick(city.name);
-        shuffleCards();
-      }}
-    />
-  ));
-
-  return <div className="all-cards">{displayCards}</div>;
-}
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [winState, setWinState] = useState(false);
+
+  console.log("App is rendering! Score is " + score)
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score)
+    }
+
+    if (score == citiesObject.length) {
+      setWinState(true);
+    }
+
+  }, [score]);
+
+  useEffect(()=> {
+    if (winState) {
+      alert("You won!")
+      setWinState(false)
+    }
+  }, [winState])
 
   return (
     <div className="app">
       <header>
         <h1 className="title">Memory Cards: Canada Edition</h1>
         <p className="instructions">
-          Click on each Canadian city or territory once (and only once) to win.
+          Click on each Canadian city or territory once (and only once) to win
         </p>
         <ScoreBoard score={score} bestScore={bestScore} />
-        <Cards />
+        <Cards score={score} setScore={setScore} winState={winState}/>
       </header>
     </div>
   );
---
+++ src/Components/Cards.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react';
+import citiesObject from '../cities';
+import '../Styles/Cards.css';
+
+function Card({ city, index, url, handleClick }) {
+  return (
+    <figure className="city-card" onClick={handleClick}>
+      <img
+        src={url}
+        alt={city}
+      />
+      <figcaption>{city}</figcaption>
+    </figure>
+  );
+}
+
+export default function Cards({ score, setScore, winState }) {
+  const [history, setHistory] = useState([]);
+  const [orderedCities, setOrderedCities] = useState(citiesObject);
+
+  function timeout(delay) {
+    return new Promise((res) => setTimeout(res, delay));
+  }
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  useEffect(() => {
+    if (winState) {
+      setHistory([]);
+      setScore(0);
+    }
+  }, [winState]);
+
+
+  function recordClick(city) {
+    const repeats = history.filter((entry) => entry == city);
+    if (repeats.length == 0) {
+      setScore((prev) => prev + 1);
+      setHistory((prevHistory) => [...prevHistory, city]);
+    } else {
+      setScore(0);
+      setHistory([]);
+    }
+  }
+
+  async function shuffleCards() {
+    // Fisher-Yates; did not think it up myself
+    function randomSort(array) {
+      // Starting at last element
+      for (let i = array.length - 1; i > 0; i--) {
+        // Pick a random element from earlier in the array
+        let j = Math.floor(Math.random() * (i + 1));
+        // swap with current elements
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
+
+    const shuffledCities = randomSort(citiesObject).map((city) => ({
+      ...city,
+    }));
+
+    await timeout(400);
+    setOrderedCities(shuffledCities);
+  }
+
+  const displayCards = orderedCities.map((city) => (
+    <Card
+      city={city.name}
+      key={city.index}
+      index={city.index}
+      url={city.url}
+      handleClick={() => {
+        recordClick(city.name);
+        shuffleCards();
+      }}
+    />
+  ));
+
+  return <div className="all-cards">{displayCards}</div>;
+}
